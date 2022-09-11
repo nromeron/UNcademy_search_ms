68be47974cc61de48def3f83179a8db44afb5302
nromeron <nromeron@unal.edu.co>
2022-09-10 21:37:30 -0500

    Por fin algo funciona

---
+++ ms_buscador/seeders/20220911004702-sede.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Sedes', [{
+      id:1,
+      nombre: "Bogotá",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {id:2,
+      nombre: "Medellín",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {id:3,
+      nombre: "manizales",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('CartItems', null, {});
+  }
+};
--- ms_buscador/server.js
+++ ms_buscador/server.js
@@ -1,46 +1,42 @@
-const express = require('express')
+const express = require('express');
+const logger = require('morgan');
 const bodyParser = require('body-parser');
-const db = require('./models/index.js')
+const db = require('./models/index')
 const cors = require('cors')
 var shell = require("shelljs");
 
 async function updateDatabase() {
-    await db.sequelize.sync({ force: true });
+     await db.sequelize.sync({ force: true }).then(() => {
+        console.log('yes re-sync done!');
+})
 }
 var argumentsArr = process.argv
 
 async function update() {
-    await updateDatabase();
-    shell.exec("npx sequelize-cli db:seed:all");
-    console.log("Models updated");
-    process.exit()
+     await updateDatabase();
+     shell.exec("npx sequelize-cli db:seed:all");
+     console.log("Models updated");
+     process.exit()
 }
 if (argumentsArr[2] == "updateDatabase") {
-    update();
+     update();
 }
 
-const app = express()
-
-app.use(express.json())
+const http = require('http');
+const app = express();
+app.use(cors());
 app.use(bodyParser.json());
+
 app.use(express.urlencoded({ extended: true }))
 
+//require("./routes")(app);
 
-// routers
-const sedeRouter = require('./routes/sedeRouter')
-app.use('/buscador', sedeRouter)
-
-
-//port
-
-app.get("/", (req,res) =>{
-    res.json({message: 'funciona carajoooo'})
-})
-
-const PORT = process.env.PORT || 8080
-
-//server
-
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+app.get('/', (req, res) => res.status(200).send({
+     message: 'Bienvenido a alguna mierda',
+}));
+const port = parseInt(process.env.PORT, 10) || 8080;
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+app.use(logger('dev'));
+module.exports = app;
